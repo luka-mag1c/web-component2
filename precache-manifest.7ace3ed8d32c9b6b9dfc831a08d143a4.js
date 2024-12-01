1ef6951c37f52377db5cd49f4e3f31b080de0730
Kvas <nikit2232@gmail.com>
2024-12-01 16:47:17 +0000

    first commit

---
+++ precache-manifest.7ace3ed8d32c9b6b9dfc831a08d143a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebce81ad9389f01b826337d1f14b665",
+    "url": "/web-component2/index.html"
+  },
+  {
+    "revision": "8004d2e8387c85e69c2c",
+    "url": "/web-component2/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "ee472f4fd4688763f161",
+    "url": "/web-component2/static/js/2.5f89583f.chunk.js"
+  },
+  {
+    "revision": "8004d2e8387c85e69c2c",
+    "url": "/web-component2/static/js/main.662e44fb.chunk.js"
+  },
+  {
+    "revision": "3cd6a68f04114b8bccf6",
+    "url": "/web-component2/static/js/runtime~main.fca892d1.js"
+  }
+]);
